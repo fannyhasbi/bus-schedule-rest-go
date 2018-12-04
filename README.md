405b782d5126cf8b74010ba2b86e61eb074a624d
fannyhasbi <fnyhsbi@gmail.com>
2018-12-04 22:41:12 +0700

    Init a web

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Bus Schedule REST Go
-RESTful website built with Go to support [Bus](https://github.com/fannyhasbi/bus-schedule-react)
+RESTful website built with Go to support [Bus Schedule React](https://github.com/fannyhasbi/bus-schedule-react)
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("Hello world");
+const PORT = ":8080"
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	response := "This is web service for Bus Schedule React"
+	fmt.Fprintln(w, response)
+}
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	err := http.ListenAndServe(PORT, nil)
+
+	if err != nil {
+		fmt.Println("Error")
+	}
 }
